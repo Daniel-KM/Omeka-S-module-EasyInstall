c8ae5fe8e3ffe460155ba6405cce5687c6b42083
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-10-31 00:00:00 +0000

    Released version 3.3.7 for archive.

--- Module.php
+++ Module.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 /*
- * Copyright 2017-2021 Daniel Berthereau
+ * Copyright 2017-2022 Daniel Berthereau
  *
  * This software is governed by the CeCILL license under French law and abiding
  * by the rules of distribution of free software. You can use, modify and/or
@@ -29,12 +29,36 @@
 
 namespace EasyInstall;
 
+use Laminas\ServiceManager\ServiceLocatorInterface;
 use Omeka\Module\AbstractModule;
+use Omeka\Stdlib\Message;
 
 class Module extends AbstractModule
 {
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+     public function install(ServiceLocatorInterface $services): void
+     {
+         $this->warnDeprecation($services);
+     }
+
+     public function upgrade($oldVersion, $newVersion, ServiceLocatorInterface $services)
+     {
+         $this->warnDeprecation($services);
+     }
+
+     protected function warnDeprecation($services): void
+     {
+        $messenger = $services->get('ControllerPluginManager')->get('messenger');
+        $message = new Message(
+            'This module is deprecated and has been superceded by %1$sEasy Admin%2$s. The upgrade from it is automatic.', // @translate
+            '<a href="https://gitlab.com/Daniel-KM/Omeka-S-module-EasyAdmin" target="_blank">',
+            '</a>'
+        );
+        $message->setEscapeHtml(false);
+        $messenger->addWarning($message);
     }
 }
